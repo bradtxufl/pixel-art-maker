ef44cf708ef26a3e281b0ddd9d14b8e64caf003c
bradtxufl <brad.w@utexas.edu>
2019-05-09 15:10:44 -0700

    pretty pretty pixels works for now

--- script.js
+++ script.js
@@ -9,10 +9,24 @@ for (var i = 0; i < boxes.length; i++) {
 
     if (brushColor == 'red') {
     e.target.className = 'col red';
+  } else if (brushColor == 'orange') {
+    e.target.className = 'col orange';
   } else if (brushColor == 'yellow') {
     e.target.className = 'col yellow';
-  } else {
+  } else if (brushColor == 'green') {
+    e.target.className = 'col green';
+  } else if (brushColor == 'blue') {
     e.target.className = 'col blue';
+  } else if (brushColor == 'purple') {
+    e.target.className = 'col purple';
+  } else if (brushColor == 'grey') {
+    e.target.className = 'col grey';
+  } else if (brushColor == 'brown') {
+    e.target.className = 'col brown';
+  } else if (brushColor == 'white') {
+    e.target.className = 'col white';
+  } else {
+    e.target.className = 'col black';
   }
   })
 }
@@ -22,6 +36,8 @@ var buttons = document.querySelectorAll('button');
 for (var j = 0; j < buttons.length; j++) {
   buttons[j].addEventListener('click', (e) => {
     brushColor = e.target.id;
+    buttons[buttons.length - 1].className = 'btn btn-outline-success ' + e.target.id;
+    console.log(brushColor);
   })
 }
 
